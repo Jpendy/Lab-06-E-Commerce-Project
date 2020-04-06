d925947d3f5834a9f8aca1226bced0bd7fc51cb9
Jpendy <Jpendy256@gmail.com>
2020-04-06 14:35:57 -0700

    set up one product, rendering with the javascript appending stuff

---
+++ pokemonArray.js
@@ -0,0 +1,67 @@
+
+export const pokemonArray = [
+    
+    {
+        id: 1,
+        name: "Ash's Charizard",
+        lowerName: 'charizard',
+        image: 'https://vignette.wikia.nocookie.net/iso33private/images/9/95/Charizard.png/revision/latest/scale-to-width-down/340?cb=20170727171344',
+        category: 'Fire / Flying / Dragon',
+        description: 'Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself.',
+        price: '$5.00'
+    },
+
+    // {
+    //     id: 2,
+    //     name: "Ash's Butterfree",
+    //     lowerName: 'butterfree',
+    //     image: 'https://vignette.wikia.nocookie.net/pokemon-adventures-spinoff-series/images/c/cf/012_butterfree_female_by_tzblacktd_da0qcev-fullview.png/revision/latest?cb=20190922021700',
+    //     category: 'Bug / Flying',
+    //     description: 'Butterfree has a superior ability to search for delicious honey from flowers. It can even search out, extract, and carry honey from flowers that are blooming over six miles from its nest.'
+    // },
+
+    // {
+    //     id: 3,
+    //     name: "Ash's Pidgeotto",
+    //     lowerName: 'pidgeotto',
+    //     image: 'https://i.pinimg.com/originals/49/57/2d/49572d5ea2becc1379b06337a32c1887.png',
+    //     category: 'Normal / Flying',
+    //     description: 'Pidgeotto claims a large area as its own territory. This Pokémon flies around, patrolling its living space. If its territory is violated, it shows no mercy in thoroughly punishing the foe with its sharp claws.'
+    // },
+
+    // {
+    //     id: 4,
+    //     name: "Ash's Pikachu",
+    //     lowerName: 'pikachu',
+    //     image: 'https://webstockreview.net/images/pikachu-clipart-small-4.png',
+    //     category: 'Electric',
+    //     description: "Whenever Pikachu comes across something new, it blasts it with a jolt of electricity. If you come across a blackened berry, it's evidence that this Pokémon mistook the intensity of its charge."
+    // },
+
+    // {
+    //     id: 5,
+    //     name: "Ash's Bulbasaur",
+    //     lowerName: 'bulbasaur',
+    //     image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+    //     category: 'Grass',
+    //     description: "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger."
+    // },
+
+    // {
+    //     id: 6,
+    //     name: "Ash's Squirtle",
+    //     lowerName: 'squirtle',
+    //     image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+    //     category: 'Water',
+    //     description: "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds."
+    // },
+
+    // {
+    //     id: 7,
+    //     name: 'Raticate',
+    //     lowerName: 'raticate',
+    //     image: 'https://static.pokemonpets.com/images/monsters-images-800-800/20-Raticate.png',
+    //     category: 'Normal',
+    //     description: "Raticate's sturdy fangs grow steadily. To keep them ground down, it gnaws on rocks and logs. It may even chew on the walls of houses."
+    // },
+];
