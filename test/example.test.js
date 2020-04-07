dee8c657edb8d397feae321c0d1642e6f27c3698
Jpendy <Jpendy256@gmail.com>
2020-04-06 17:03:16 -0700

    passed the test for the render function

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,32 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
 
+import { renderPokemon } from '../render-pokemon.js'; 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+ test('time to test a function', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-
+    const charizard = {
+        id: 1,
+        name: "Ash's Charizard",
+        lowerName: 'charizard',
+        image: 'https://vignette.wikia.nocookie.net/iso33private/images/9/95/Charizard.png/revision/latest/scale-to-width-down/340?cb=20170727171344',
+        category: 'Fire / Flying / Dragon',
+        description: 'Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself.',
+        price: '$2.00'
+    };
+ 
+    const expected = `<li style="list-style: none;"><h2>Ash's Charizard</h2><img src="https://vignette.wikia.nocookie.net/iso33private/images/9/95/Charizard.png/revision/latest/scale-to-width-down/340?cb=20170727171344" style="height: 250px; width: 250px;"><p>Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself.</p><h2>$2.00</h2><h3>Fire / Flying / Dragon</h3><button value="1">Add to Cart</button></li>`;
+    
+    
     //Act 
     // Call the function you're testing and set the result to a const
+    const el = renderPokemon(charizard);
+
+    const outerHTML = el.outerHTML;
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(expected, outerHTML);
 });
