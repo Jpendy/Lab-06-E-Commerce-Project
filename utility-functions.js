a0b4e0903bc158f750d86ecd0a8fab76a30847f0
Jpendy <Jpendy256@gmail.com>
2020-04-07 14:27:45 -0700

    created findById function and calcLineitem function

---
+++ utility-functions.js
@@ -0,0 +1,17 @@
+export function findById(pokemonArray, cartArray) {
+    for (let i = 0; i < pokemonArray.length; i++) {
+        let array = pokemonArray[i];
+
+        if (array.id === cartArray.id) {
+            return array.name;
+        }
+    }
+}
+
+
+
+export function calcLineItem(quantity, price) {
+    let lineTotal = quantity * price;
+    return lineTotal;
+
+}
