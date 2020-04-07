ae610de7f3c475e32f722b8d299aabd9bb73bf5a
Jpendy <Jpendy256@gmail.com>
2020-04-07 13:57:53 -0700

    hardcoded html table template and created cartArray.js and filled created cartArray

---
+++ cartArray.js
@@ -0,0 +1,36 @@
+export cartArray = [
+    {
+        id: 1,
+        quantity: 2
+    },
+
+    {
+        id: 2,
+        quantity: 4
+    },
+
+    {
+        id: 3,
+        quantity: 3
+    },
+
+    {
+        id: 4,
+        quantity: 5
+    },
+
+    {
+        id: 5,
+        quantity: 2
+    },
+
+    {
+        id: 6,
+        quantity: 3
+    },
+
+    {
+        id: 7,
+        quantity: 6
+    }
+];
