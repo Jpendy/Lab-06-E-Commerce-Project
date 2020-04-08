3c69a6d4d7ab64dd2587ce7960754c4d95725429
Jpendy <Jpendy256@gmail.com>
2020-04-08 14:11:09 -0700

    working on the local storage, got a bug to fix but it's close

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-import { pokemonArray } from './pokemonArray.js';
+import pokemonArray from './pokemonArray.js';
 import { renderPokemon } from './render-pokemon.js';
 
 const mainSection = document.getElementById('main-section');
 
 for (let i = 0; i < pokemonArray.length; i++){
+    
     const pokemon = pokemonArray[i];
 
     const list = renderPokemon(pokemon);
--- render-pokemon.js
+++ render-pokemon.js
@@ -1,3 +1,6 @@
+import { findById } from './utility-functions.js';
+//import pokemonArray from './pokemonArray.js';
+
 export function renderPokemon(pokemon) {
     const list = document.createElement('li');
     const name = document.createElement('h2');
@@ -6,16 +9,50 @@ export function renderPokemon(pokemon) {
     const price = document.createElement('h2');
     const category = document.createElement('h3');
     const button = document.createElement('button');
+
     name.textContent = pokemon.name;
     img.src = pokemon.image;
-    img.style.height = '250px';
-    img.style.width = '250px';
+    img.style.height = '200px';
+    img.style.width = '200px';
     description.textContent = pokemon.description;
-    price.textContent = pokemon.price;
+    price.textContent = '$' + pokemon.price.toFixed(2);
     category.textContent = pokemon.category;
     list.style.listStyle = 'none';
     button.value = pokemon.id;
     button.textContent = 'Add to Cart';
+    button.addEventListener('click', () => {
+
+        let cartItem = localStorage.getItem('CART');
+        let cart;
+        console.log(cartItem);
+        if (cartItem) {
+            cart = JSON.parse(cartItem);
+        }
+        else {
+            cart = [];
+        }
+        
+        let item = findById(cart, pokemon.id);
+
+        if (!item) {
+            item = {
+                id: pokemon.id,
+                quantity: 1
+            };
+
+            cart.push(item);
+        }
+        else {
+            item.quantity++;
+        }
+
+        cartItem = JSON.stringify(cart);
+        localStorage.setItem('CART', item);
+
+        alert('1 ' + pokemon.name + ' added to cart');
+
+    });
+   
     list.append(name, img, description, price, category, button);
     return list;
 }
